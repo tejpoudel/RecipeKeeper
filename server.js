b2d36f6d890a3a49bfe88b5428691df1fde30927
w.benarto <w.benarto@gmail.com>
2019-01-15 10:26:28 -0800

    controllers, schema, seeds, connection.js

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ app.get("/", function(req, res) {
 
 const PORT = process.env.PORT || 5080;
 
+// Sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Establishing Handlebars as templating language
+const exphbs = require('express-handlebars');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Importing routes and giving access to it
+const routes = require('./controllers/recipe_controller');
+
+app.use('/', routes);
+
 app.listen(PORT, function() {
     console.log("Server is running on " + PORT);
 });
-
 
 
 
